690b6e60e688a1ff0c35b1ee26e123f50cf8b373
victor <victorm922@hotmail.com>
2024-01-09 20:48:07 -0500

    pareur

---
+++ main/parseur.py
@@ -0,0 +1,35 @@
+class Parse:
+
+    def __init__(self,main):
+        self.main = main
+
+
+    def Identifier_infos(self):
+        utg = ""
+        utg_1 = ""
+        utg_2 = ""
+        lowjack = ""
+        highjack = ""
+        cutoff = ""
+        button = ""
+        smallblind = ""
+        bigblind = ""
+
+        stack_utg = ""
+        stack_utg_1 = ""
+        stack_utg_2 = ""
+        stack_lowjack = ""
+        stack_highjack = ""
+        stack_cutoff = ""
+        stack_button = ""
+        stack_smallblind = ""
+        stack_bigblind = ""
+
+        action = ""
+        flop = ""
+        turn = ""
+        river = ""
+
+
+
+
