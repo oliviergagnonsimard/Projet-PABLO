f2dd4f9cba0ffcd112739731dee36e44980ee2b7
victor <victorm922@hotmail.com>
2024-01-10 01:55:34 -0500

    avancement

--- main/parser.py
+++ main/parser.py
@@ -4,6 +4,7 @@ class Parse:
 
     def __init__(self,main):
         self.main = main
+        self.nombre_joueur = self.nombre_joueur()
 
 
     def Identifier_infos(self):
@@ -45,6 +46,7 @@ class Parse:
             else:
                 return a
     def recup_pseudo_apres(self, infos):
+
         for i in infos:
             b = 0
             if i != ":":
@@ -56,20 +58,63 @@ class Parse:
         self.num_seat_bb = info[6]
 
     def recherche_position(self,num_seat_bb,pseudo):
-        if re.search(r"^Table '\w+ \w+' 6-max",self.main):
+       # if re.search(r"^Table '\w+ \w+' 6-max",self.main):
+       for i in range(int(self.nombre_joueur)):
             no_seat = int(re.search(fr"^ Seat \w:{pseudo}",self.main).group()[6])
-            if no_seat == num_seat_bb+1 or no_seat == num_seat_bb-5:
-                lowjack = pseudo
-            elif no_seat == num_seat_bb+2 or no_seat == num_seat_bb-4:
-                highjack = pseudo
-            elif no_seat == num_seat_bb+3 or no_seat == num_seat_bb-3:
-                cutoff = pseudo
-            elif no_seat == num_seat_bb+4 or no_seat == num_seat_bb-2:
+            if no_seat == num_seat_bb+4 or no_seat == num_seat_bb-(self.nombre_joueur-4):
                 button = pseudo
-        if re.search(r"^Table '\w+ \w+' 9-max",self.main):
-            pass
+            elif no_seat == num_seat_bb+3 or no_seat == num_seat_bb-(self.nombre_joueur-3):
+                cutoff = pseudo
+            elif no_seat == num_seat_bb+2 or no_seat == num_seat_bb-(self.nombre_joueur-2):
+                highjack = pseudo
+            elif no_seat == num_seat_bb+1 or no_seat == num_seat_bb-(self.nombre_joueur-1):
+                lowjack = pseudo
 
 
+    def nombre_joueur(self):
+        occurrences_joueurs = re.findall(re.compile(r'Seat \d+: '), self.main)
+        return len(occurrences_joueurs)/2
+
+
+
+historique_main = """PokerStars Hand #248175750958:  Hold'em No Limit ($0.01/$0.02 USD) - 2024/01/09 19:32:13 ET
+Table 'Alhena III' 6-max Seat #5 is the button
+Seat 1: andy_keiv ($1.97 in chips)
+Seat 2: RReyesJ ($1.41 in chips)
+Seat 3: ULISSES 222 ($1.69 in chips)
+Seat 5: altazorchile ($1.58 in chips)
+Seat 6: PokerBoss2639 ($2.07 in chips)
+PokerBoss2639: posts small blind $0.01
+andy_keiv: posts big blind $0.02
+*** HOLE CARDS ***
+RReyesJ: folds
+ULISSES 222: folds
+altazorchile: calls $0.02
+PokerBoss2639: folds
+andy_keiv: checks
+*** FLOP *** [6c Th 2c]
+andy_keiv: checks
+altazorchile: checks
+*** TURN *** [6c Th 2c] [2d]
+Zeydn13 joins the table at seat #4
+andy_keiv: bets $0.03
+altazorchile: calls $0.03
+*** RIVER *** [6c Th 2c 2d] [7d]
+ULISSES 222 leaves the table
+andy_keiv: checks
+altazorchile: checks
+*** SHOW DOWN ***
+andy_keiv: shows [8d Qs] (a pair of Deuces)
+altazorchile: shows [5d 6d] (two pair, Sixes and Deuces)
+altazorchile collected $0.11 from pot
+*** SUMMARY ***
+Total pot $0.11 | Rake $0
+Board [6c Th 2c 2d 7d]
+Seat 1: andy_keiv (big blind) showed [8d Qs] and lost with a pair of Deuces
+Seat 2: RReyesJ folded before Flop (didn't bet)
+Seat 3: ULISSES 222 folded before Flop (didn't bet)
+Seat 5: altazorchile (button) showed [5d 6d] and won ($0.11) with two pair, Sixes and Deuces
+Seat 6: PokerBoss2639 (small blind) folded before Flop"""
 
 
 
